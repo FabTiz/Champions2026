e81847e280936a26cdeecfec8b873ae9dd0f25d3
FabTiz <fabtiz91@gmail.com>
2026-08-09 18:34:46 +0200

    Clean App.jsx: removed testConnection

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,6 @@ import { supabase } from './supabaseClient';
 import InsertLeagueTurn from "./components/InsertLeagueTurn";
 
 function App() {
-  async function testConnection() {
-    const { data, error } = await supabase.from('test').select('*');
-    console.log("DATA:", data);
-    console.log("ERROR:", error);
-  }
-
-  testConnection();
-
   return (
     <>
       <h1>Champions League Comp</h1>
@@ -19,4 +11,3 @@ function App() {
 }
 
 export default App;
-
